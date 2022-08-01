28e8e3823f200ef1a881d630d36ab1740bf4a414
geru-v-venu <germanboyaksky@gmail.com>
2022-08-01 15:56:16 +0300

    added DragAndDropTest

---
+++ src/test/java/guru/qa/DragAndDropTest.java
@@ -0,0 +1,19 @@
+package guru.qa;
+
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class DragAndDropTest {
+    @Test
+    void dragAndDrop(){
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        $("#column-a").dragAndDropTo($("#column-b"));
+        $("#column-a").shouldHave(text("B"));
+        $("#column-b").shouldHave(text("A"));
+
+
+    }
+}
